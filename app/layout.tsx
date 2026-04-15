e32ba76c43bbdb540938b2a2a9a11e22b54b7d05
mrkzbhwthalldayn-maker <mrkzbhwthalldayn@gmail.com>
2026-04-15 13:01:31 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Cairo } from "next/font/google";
-import "./globals.css";
+
 import { cn } from "@/lib/utils";
 import ThemeProvider from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
