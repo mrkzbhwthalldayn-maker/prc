34c31daa330432962da0193aeaf4ff4419bb723f
mrkzbhwthalldayn-maker <mrkzbhwthalldayn@gmail.com>
2026-04-15 11:55:32 +0200

    Update layout.tsx

--- app/[lang]/(auth)/layout.tsx
+++ app/[lang]/(auth)/layout.tsx
@@ -16,43 +16,8 @@ const Layout = ({
 
   return (
     <main className="md:flex h-screen">
-      <section className="bg-primary hidden md:block md:flex-[3] text-lg text-center content-center">
-        <div>
-          <div className="h-full w-full md:w-1/2 md:h-1/2 mx-auto">
-            <Image
-              src="/logo.png"
-              width={1000}
-              height={1000}
-              alt="logo"
-              className="w-full h-full object-cover"
-              priority
-            />
-          </div>
-
-          <h2 className="text-2xl text-white text-center font-bold" id="quote">
-            <LangRenderer
-              ar={`"لا تنتظر الوقت المناسب، اصنعه بنفسك ."`}
-              en={`"Don't wait for the right time, make it yourself."`}
-            />
-          </h2>
-        </div>
-      </section>
-
-      <section className="md:flex-[2] flex-col flex justify-center gap-5 h-full relative">
-        <CustomLink
-          size={"icon"}
-          variant={"secondary"}
-          className={cn(
-            "absolute top-2",
-            lang === "ar" ? "right-2" : "left-2"
-          )}
-          href={`/${lang}`}
-        >
-          <LangRenderer en={<FaArrowLeft />} ar={<FaArrowRight />} />
-        </CustomLink>
-
-        {children}
-      </section>
+      ...
+      {children}
     </main>
   );
 };
