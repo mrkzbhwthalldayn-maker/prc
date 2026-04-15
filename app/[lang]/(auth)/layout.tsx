fa528f1177c5b7a1643c14391b02f3b09976e251
mrkzbhwthalldayn-maker <mrkzbhwthalldayn@gmail.com>
2026-04-15 12:04:03 +0200

    Update layout.tsx

--- app/[lang]/(auth)/layout.tsx
+++ app/[lang]/(auth)/layout.tsx
@@ -1,26 +1,56 @@
 import { ReactNode } from "react";
-import LangRenderer from "../../../components/lang";
 import Image from "next/image";
 import { CustomLink } from "@/components/custom-link";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { cn } from "@/lib/utils";
+import LangRenderer from "../../../components/lang";
 
-const Layout = ({
+export default function Layout({
   children,
   params,
 }: {
   children: ReactNode;
   params: { lang: string };
-}) => {
+}) {
   const { lang } = params;
 
   return (
     <main className="md:flex h-screen">
-      <section>Left side</section>
+      <section className="hidden md:block md:flex-[3] bg-primary text-white text-center">
+        <div>
+          <Image
+            src="/logo.png"
+            width={1000}
+            height={1000}
+            alt="logo"
+            className="w-full h-full object-cover"
+            priority
+          />
 
-      <section>{children}</section>
+          <h2 className="text-2xl font-bold">
+            <LangRenderer
+              ar="لا تنتظر الوقت المناسب، اصنعه بنفسك."
+              en="Don't wait for the right time, make it yourself."
+            />
+          </h2>
+        </div>
+      </section>
+
+      <section className="md:flex-[2] flex flex-col justify-center relative">
+        <CustomLink
+          size="icon"
+          variant="secondary"
+          className={cn(
+            "absolute top-2",
+            lang === "ar" ? "right-2" : "left-2"
+          )}
+          href={`/${lang}`}
+        >
+          <LangRenderer en={<FaArrowLeft />} ar={<FaArrowRight />} />
+        </CustomLink>
+
+        {children}
+      </section>
     </main>
   );
-};
-
-export default Layout;
+}
