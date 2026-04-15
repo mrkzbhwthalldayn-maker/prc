a335a3251ed9c928a135ab484cac00bac915ca78
mrkzbhwthalldayn-maker <mrkzbhwthalldayn@gmail.com>
2026-04-15 11:53:08 +0200

    Update layout.tsx

--- app/[lang]/(auth)/layout.tsx
+++ app/[lang]/(auth)/layout.tsx
@@ -1,26 +1,24 @@
 import { ReactNode } from "react";
 import LangRenderer from "../../../components/lang";
-// import Logo from "../components/header/logo";
 import Image from "next/image";
 import { CustomLink } from "@/components/custom-link";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { cn } from "@/lib/utils";
 
-const layout = async (props: {
+const Layout = ({
+  children,
+  params,
+}: {
   children: ReactNode;
-  params: Promise<{ lang: string }>;
+  params: { lang: string };
 }) => {
-  const params = await props.params;
-
   const { lang } = params;
-
-  const { children } = props;
 
   return (
     <main className="md:flex h-screen">
       <section className="bg-primary hidden md:block md:flex-[3] text-lg text-center content-center">
         <div>
-          <div className=" h-full w-full md:w-1/2 md:h-1/2 mx-auto">
+          <div className="h-full w-full md:w-1/2 md:h-1/2 mx-auto">
             <Image
               src="/logo.png"
               width={1000}
@@ -30,6 +28,7 @@ const layout = async (props: {
               priority
             />
           </div>
+
           <h2 className="text-2xl text-white text-center font-bold" id="quote">
             <LangRenderer
               ar={`"لا تنتظر الوقت المناسب، اصنعه بنفسك ."`}
@@ -38,20 +37,24 @@ const layout = async (props: {
           </h2>
         </div>
       </section>
+
       <section className="md:flex-[2] flex-col flex justify-center gap-5 h-full relative">
         <CustomLink
           size={"icon"}
           variant={"secondary"}
-          className={cn("absolute top-2", lang === "ar" ? "right-2" : "left-2")}
+          className={cn(
+            "absolute top-2",
+            lang === "ar" ? "right-2" : "left-2"
+          )}
           href={`/${lang}`}
         >
           <LangRenderer en={<FaArrowLeft />} ar={<FaArrowRight />} />
         </CustomLink>
-        {/* <Logo className="w-28 block h-28 mx-auto overflow-hidden md:hidden" /> */}
+
         {children}
       </section>
     </main>
   );
 };
 
-export default layout;
+export default Layout;
