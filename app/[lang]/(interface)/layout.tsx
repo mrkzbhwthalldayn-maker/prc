a1fa408d049a06258bbe65f64011ff90889ce7ac
mrkzbhwthalldayn-maker <mrkzbhwthalldayn@gmail.com>
2026-04-16 12:36:12 +0200

    Update layout.tsx

--- app/[lang]/(interface)/layout.tsx
+++ app/[lang]/(interface)/layout.tsx
@@ -1,5 +1,5 @@
 import { Cairo, Outfit } from "next/font/google";
-import "../globals.css";
+import "@/app/globals.css";
 import { cn } from "@/lib/utils";
 
 import Footer from "./components/footer";
